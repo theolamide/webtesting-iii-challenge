15813961ac2828146654359f790a49c784c264b8
theolamide <ajadidan@gmail.com>
2019-11-12 15:08:47 -0500

    Some tests written

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Mocking for smile', () => {
+    const mock = jest.fn();
+    mock('smile');
+    expect(mock).toHaveBeenCalled();
+})
+
+
+test('Dashboard renders correctly', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import 'jest-dom/extend-expect'
+import Display from './Display';
+
+test('Display renders correctly', ()=>{
+    expect(render(<Display />)).toMatchSnapshot();
+})
+
+test('locked and closed test', ()=>{
+    const {getByText} = render (<Display locked={true} closed={true} />);
+    getByText(/^closed$/i);
+    getByText(/^locked$/i);
+});
+
+test('unlocked and open test', ()=>{
+    const {getByText} = render (<Display locked={false} closed={false} />);
+    getByText(/^open$/i);
+    getByText(/^unlocked$/i);
+});
+
